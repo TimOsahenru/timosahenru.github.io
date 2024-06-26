dd97e4c36f501e3128c519706e8b2e14b2af4c52
Osahenru <93861430+TimOsahenru@users.noreply.github.com>
2024-06-26 18:24:34 +0000

    Delete portfolio directory

--- portfolio/scripts.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".nav-toggler").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("active");
-    document.body.classList.toggle("nav-active");
-    body
-});
